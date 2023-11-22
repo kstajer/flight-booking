f527497ac17ec07002953e6d11e7d353c6559c4d
Karol Stajer <56116131+kstajer@users.noreply.github.com>
2023-11-22 20:45:21 +0100

    Delete backend directory

--- backend/airports/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Airport
-
-class AirportSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Airport 
-        fields = ('airport_id', 'airport_name', 'city', 'country')
