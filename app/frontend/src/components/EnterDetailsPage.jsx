8a2420273e14fca9e129ab0091ea95c991f28a7c
Marta Adamczyk <martaadamczyk@MacBook-Air-Marta.local>
2023-11-25 16:08:01 +0100

    add expanding content component, details page

--- app/frontend/src/components/EnterDetailsPage.jsx
+++ app/frontend/src/components/EnterDetailsPage.jsx
@@ -1,13 +1,64 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import { useSearchParams, useNavigate } from "react-router-dom";
-import axios from "axios";
-import moment from "moment-with-locales-es6";
-import Select from "react-select";
+import React, { useState } from "react";
+import ExpandingContent from "./ExpandingContent";
 
 function EnterDetailsPage() {
+  const [openIds, setOpenIds] = useState([0]);
+  const [formData, setFormData] = useState([]);
+
+  const amount = 3;
+
+  const handleToggle = (id) => {
+    setOpenIds((prevOpenIds) => {
+      if (prevOpenIds.includes(id)) {
+        return prevOpenIds.filter((openId) => openId !== id);
+      } else {
+        return [id];
+      }
+    });
+  };
+
+  const handleConfirm = () => {
+    const isFormValid = Array.from({ length: amount }, (_, index) => {
+      const inputData = formData[index];
+      return (
+        inputData &&
+        inputData.name.trim() !== "" &&
+        inputData.surname.trim() !== "" &&
+        inputData.documentNumber.trim() !== ""
+      );
+    }).every(Boolean);
+
+    if (isFormValid) {
+      alert("Confirmation successful!");
+    } else {
+      alert("Wszystkie pola muszą być wypełnione.");
+    }
+  };
+
+  const handleFormDataChange = (id, data) => {
+    setFormData((prevFormData) => {
+      const updatedFormData = [...prevFormData];
+      updatedFormData[id] = { ...updatedFormData[id], ...data };
+      return updatedFormData;
+    });
+  };
+
   return (
-    <div className="bg-white rounded-3xl w-[70%] h-fit z-20 p-12 flex flex-col items-center justify-center shadow-lg text-gray-800"></div>
+    <div className="bg-white rounded-3xl w-[70%] h-fit z-20 p-12 shadow-lg flex flex-col text-gray-800">
+      <div className="border border-gray-300 rounded shadow w-[60%]">
+        {Array.from({ length: amount }, (_, index) => (
+          <ExpandingContent
+            key={index}
+            id={index}
+            open={openIds.includes(index)}
+            onToggle={() => handleToggle(index)}
+            formData={formData[index] || {}}
+            onFormDataChange={(data) => handleFormDataChange(index, data)}
+          />
+        ))}
+      </div>
+      <button onClick={handleConfirm}>Confirm</button>
+    </div>
   );
 }
 
