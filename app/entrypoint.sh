c825c4121a26bb657706f7de93a8d87a111c27cc
kstajer <kastajer1@gmail.com>
2023-12-02 23:30:26 +0100

    Add static files

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 export DJANGO_SETTINGS_MODULE=backend.settings
 # wait for react npm run build
-while [ ! -f /frontend/build/index.html ]; do
-    sleep 2
-done
 
 python manage.py makemigrations
 python manage.py migrate
